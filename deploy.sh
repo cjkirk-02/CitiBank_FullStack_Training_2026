c580bbe420f0b541970ece85bfc64b7e6b718c1b
Cameron Kirk <kirkc0202@gmail.com>
2026-08-03 19:49:36 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# 1. Ensure we are pulling from the specific backend branch
-# (Replace 'backend' with your exact branch name if different)
+cd /home/ec2-user/CitiBank_FullStack_Training_2026
 git checkout REST-API
 git pull origin REST-API
 
-# 2. Activate Python virtual environment & install dependencies
-source /home/ec2-user/Citi_RestAPI/venv/bin/activate
+source venv/bin/activate
 pip install -r requirements.txt
 
-# 3. Restart the background service
 sudo systemctl restart flaskapp
